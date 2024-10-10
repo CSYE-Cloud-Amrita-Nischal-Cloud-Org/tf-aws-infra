6dea060b19135988ccd5d5704bc36d9f233e9921
Amrita Nischal <nischal.a@northeastern.edu>
2024-10-10 12:46:06 -0400

    Add subnets, routing table and igw

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,9 +1,53 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "> 5.0.0, <6.0.0"
-    }
-  }
-  required_version = ">= 1.2.0"
+variable "vpc_identifier" {
+  description = "The user-defined name for the Virtual Private Cloud (VPC)"
+  type        = string
 }
+
+variable "vpc_cidr_block" {
+    description = "The CIDR block defining the IP address range for the Virtual Private Cloud (VPC)"
+    type        = string
+    default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_blocks" {
+    description = "List of CIDR blocks for the public subnets within the VPC"   
+    type        = list(string)
+    default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_blocks" {
+    description = "List of CIDR blocks for the private subnets within the VPC"
+    type        = list(string)
+    default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones_list" {
+    description = "A list of Availability Zones in which to deploy resources within the VPC"
+    type        = list(string)
+}
+
+variable "igw_name" {
+  description = "The user-defined name for the Internet Gateway associated with the VPC"
+  type        = string
+}
+
+variable "public_subnet_route_table" {
+  description = "The user-defined name for the Public Routing Table associated with the VPC"
+  type        = string
+}
+
+variable "public_cidr_routing_table"{
+    description = "CIDR block used for routing public traffic in the routing table"
+    type        = string
+}
+
+variable "private_subnet_route_table"{
+    description = "The user-defined name for the private routing table associated with the VPC"
+    type        = string
+}  
+
+variable "aws_current_region"{
+    description = "The AWS region where resources will be deployed"
+    type        = string
+}
+
