ae0d063b9869aeb421ff3c89eadbfaa2fe790675
Amrita Nischal <nischal.a@northeastern.edu>
2024-10-15 16:40:04 -0400

    terraform validate check fail

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_identifier" {
-  description =  "The user-defined name for the Virtual Private Cloud (VPC)"
-  type        =  string
+  description = "The user-defined name for the Virtual Private Cloud (VPC)"
+  type        = string
 }
 
 variable "vpc_cidr_block" {
@@ -12,7 +12,7 @@ variable "vpc_cidr_block" {
 variable "public_subnet_blocks" {
   description = "List of CIDR blocks for the public subnets within the VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = 
 }
 
 variable "private_subnet_blocks" {
